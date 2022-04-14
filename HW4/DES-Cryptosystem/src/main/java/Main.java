21c2f82dc2f81afbd71abd38c8415a413be7e18e
Paul513607 <paul.samson42@gmail.com>
2022-04-14 23:31:14 +0300

    HW4 Initial commit

---
+++ HW4/DES-Cryptosystem/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+
+import descryptosystem.*;
+import util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ HW4/DES-Cryptosystem/src/main/java/descryptosystem/DESCryptosystem.java
@@ -0,0 +1,4 @@
+package descryptosystem;
+
+public class DESCryptosystem {
+}
